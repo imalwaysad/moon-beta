f3d15288bee2e4ff78adabea64132a6726b878e9
imalwaysad <142375208+imalwaysad@users.noreply.github.com>
2023-12-09 23:11:01 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,3 +21,8 @@ export default function Home() {
     </div>
   )
 }
+<title>1v1.lol</title>
+<link rel="icon" href="/img/JMWfavicon.png" />
+	<link rel="shortcut icon" href="/img/JMWfavicon.png" />
+	<link rel="stylesheet" href="/style.css">
+<iframe src="https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/ko1ov/jd83hf02ld65la32mdl4@44ac320eee35a6f332b3c07c0a81e611267e640c/1v1.xml" style="position:fixed;top:0;bottom:0;left:0;right:0;width:100%;height:100%;border:none;margin:0;padding:0;z-index:999999;">Your browser does not support iframes</iframe>
